b7ead89761bf4538117efaaeb6529833d62af8b9
Muhamad Dwi Cahyo <100329714+dwicahyo1512@users.noreply.github.com>
2026-04-03 19:59:01 +0700

    feat: add allowed hosts for server and preview configurations in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     port: 5175,
+    allowedHosts: ['weddingkezia.cahyo.site', 'weddingkeziav2.cahyo.site']
+  },
+  preview: {
+    allowedHosts: ['weddingkezia.cahyo.site', 'weddingkeziav2.cahyo.site']
   },
   plugins: [react()],
 });
