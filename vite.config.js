5850549f8bf683b24e4bdb824d052508a3dda9bf
Muhamad Dwi Cahyo <100329714+dwicahyo1512@users.noreply.github.com>
2026-04-03 19:15:09 +0700

    update: fix vite

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     port: 5175,
+    allowedHosts: ['weddingkezia.cahyo.site']
+  },
+  preview: {
+    allowedHosts: ['weddingkezia.cahyo.site']
   },
   plugins: [react()],
 });
